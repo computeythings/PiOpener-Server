bd25752a8ed90ec88e9021373ddd56dcf378bcf1
computeythings <b.gonnella94@gmail.com>
2017-12-11 08:33:32 -0800

    Inital commit

---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+import json
+from os import getuid
+from random import randint
+from subprocess import call
+
+if getuid() != 0:
+    print('This script must be run as root.')
+    quit()
+
+ALPHANUM = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+apikey = ''
+
+print('Using GPIO.BOARD layout');
+
+while True:
+    try:
+        relay_pin = int(input('Which pin was connected to the relay? '))
+        break
+    except ValueError:
+        print('Value must be an int.')
+while True:
+    try:
+        open_pin = int(input('Which pin was connected to the OPEN switch? '))
+        break
+    except ValueError:
+        print('Value must be an int.')
+while True:
+    try:
+        close_pin = int(input('Which pin was connected to the CLOSE switch? '))
+        break
+    except ValueError:
+        print('Value must be an int.')
+
+print('Generating API key...')
+for i in range(25):
+   apikey = apikey + ALPHANUM[randint(0,61)] 
+
+print('./src/Updating config.json')
+
+with open('./src/config.json', 'r+') as f:
+    data = json.load(f)
+    data['APIKEY'] = apikey
+    data['RELAY_PIN'] = relay_pin
+    data['OPEN_SWITCH_PIN'] = open_pin
+    data['CLOSED_SWITCH_PIN'] = close_pin
+    f.seek(0)
+    json.dump(data, f, indent=4)
+    f.truncate()
+
+print('Installing dependencies')
+call(['apt', 'install', 'python3-pip'])
+call(['pip3', 'install', 'RPi.GPIO'])
+print('Setting permissions')
+call(['chmod', '600', './src/config.json']) # Don't want anyone seeing our API key
+print('Migrating to /opt')
+call(['cp', '-r', '.', '/opt'])
+print('Creating systemd service')
+call(['mv', './src/garageopener.service', 
+        '/lib/systemd/system/garageopener.service'])
+call(['systemctl', 'daemon-reload'])
+call(['systemctl', 'enable', 'garageopener.service'])
+call(['systemctl', 'start', 'garageopener.service'])
+
+print('Complete!')
+print('\nYour API key is:\n\n{}'.format(apikey))
+print('\nMake sure you write it down in a safe place\n')
+
---
+++ src/gopener.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from time import sleep
+import json
+import logging
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+    APIKEY = config['APIKEY']
+    RELAY_PIN = config['RELAY_PIN']
+    OPEN_PIN = config['OPEN_SWITCH_PIN']
+    CLOSED_PIN = config['CLOSED_SWITCH_PIN']
+
+IS_OPEN = False
+IS_CLOSED = True # inital state is assumed to be closed
+OPENING = False
+CLOSING = False
+
+# Setup GPIO pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+GPIO.setup(OPEN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(CLOSED_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+
+
+class OpenerServer(BaseHTTPRequestHandler):
+    def _set_response(self, code):
+        self.send_response(code)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info('GET request,\nPath: %s\nHeaders:\n%s\n', str(self.path), 
+                    str(self.headers))
+        self._set_response(404)
+        self.wfile.write('This link does not support that type of request.'
+                        .encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # Gets data size
+        post_data = self.rfile.read(content_length) # Gets the data itself
+        jdata = json.loads(post_data.decode('utf-8'))
+
+        self._set_response(200)
+
+        if self.path == '/api' or self.path == '/api/':
+            if jdata['apikey'] == APIKEY:
+                if jdata['intent'] == 'OPEN':
+                    self.wfile.write('Opening garage'.encode('utf-8'))
+                    self.open_garage()
+                elif jdata['intent'] == 'CLOSE':
+                    self.wfile.write('Closing garage'.encode('utf-8'))
+                    self.close_garage()
+                elif jdata['intent'] == 'TOGGLE':
+                    self.wfile.write('Toggling garage'.encode('utf-8'))
+                    self.toggle_garage()
+                else:
+                    self.wfile.write('Invalid Command'.encode('utf-8'))
+            else:
+                self.wfile.write('Invalid API Key'.encode('utf-8'))
+        else:
+            self.wfile.write('INVALID URL'.encode('utf-8'))
+
+        jdata['apikey'] = '******'
+
+        logging.info('POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n',
+                    str(self.path), str(self.headers), jdata)
+
+    # Quickly toggle a relay closed and open to simulate a button press
+    def toggle_garage(self):
+        GPIO.output(RELAY_PIN, GPIO.HIGH)
+        sleep(0.2)
+        GPIO.output(RELAY_PIN, GPIO.LOW)
+
+    """ 
+    Since there's no open/close wire to connect to, we can only toggle the door.
+    A software check is run to determine whether or not we should toggle
+    given each command.
+
+    If the garage is in a middle state (i.e. not fully open OR closed) when the
+    command is run, the door will be toggled once and then again if the desired
+    state was not reached.
+    """
+    def open_garage(self):
+        logging.info('Opening garage');
+        if IS_OPEN:
+            return
+
+        OPENING = True # set intent
+        CLOSING = False
+        self.toggle_garage()
+        IS_CLOSED = False
+
+    def close_garage(self):
+        logging.info('Closing garage');
+        if IS_CLOSED:
+            return
+
+        CLOSING = True # set intent
+        OPENING = False
+        self.toggle_garage();
+        IS_OPEN = False
+
+    # This is run when the open switch is triggered
+    def opened(channel):
+        logging.info('Garage is now open')
+        IS_OPEN = True
+        OPENING = False
+
+        # toggle again if intent was to close
+        if CLOSING:
+            self.close_garage()
+
+    # This is run when the closed switch is triggered
+    def closed(channel):
+        logging.info('Garage is now closed')
+        IS_CLOSED = True
+        CLOSING = False
+
+        # toggle again if intent was to open
+        if OPENING:
+            self.open_garage()
+
+
+def run(server_class=HTTPServer, handler_class=OpenerServer, port=8080, 
+        logf='/var/log/gopener.log'):
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+    GPIO.add_event_detect(OPEN_PIN, GPIO.FALLING, callback=handler_class.opened, 
+                            bouncetime=300)
+    GPIO.add_event_detect(CLOSED_PIN, GPIO.FALLING, callback=handler_class.closed, 
+                            bouncetime=300)
+
+    logging.basicConfig(level=logging.INFO, 
+                        format='[%(asctime)s] %(levelname)-8s: '
+                        + '%(message)s',
+                        datefmt='%m-%d %H:%M:%S',
+                        filename=logf)
+
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    logging.warning('The first command WILL toggle garage regardless of position')
+    httpd.serve_forever()
+
+    GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    elif len(argv) == 3:
+        run(port=int(argv[1]), logf=argv[2])
+    else:
+        run()
