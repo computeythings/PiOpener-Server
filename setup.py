df5f5d47cec24dbb30ae0b237b26735dcd4e313b
computeythings <25091077+computeythings@users.noreply.github.com>
2018-02-23 03:39:22 -0800

    Updated setup.py to work for updates as well as new installs

--- setup.py
+++ setup.py
@@ -11,37 +11,53 @@ if getuid() != 0:
 ALPHANUM = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 apikey = ''
 
-print('Using GPIO.BOARD layout');
-
-while True:
-    try:
-        relay_pin = int(input('Which pin was connected to the relay? '))
-        break
-    except ValueError:
-        print('Value must be an int.')
-while True:
-    try:
-        open_pin = int(input('Which pin was connected to the OPEN switch? '))
-        break
-    except ValueError:
-        print('Value must be an int.')
-while True:
-    try:
-        close_pin = int(input('Which pin was connected to the CLOSE switch? '))
-        break
-    except ValueError:
-        print('Value must be an int.')
-        break
-
-print('Generating API key...')
-for i in range(25):
-   apikey = apikey + ALPHANUM[randint(0,len(ALPHANUM)-1)]
-
 print('./src/Updating config.json')
 
 with open('./src/config.json', 'r+') as f:
     data = json.load(f)
-    data['ACCESS_TOKEN'] = apikey
+
+    # Option to update pin layout
+    if not data['RELAY_PIN'] == '':
+        answer = input('Update the relay pin value? (Y/N)')
+        if answer.upper() == 'Y':
+            data['RELAY_PIN'] = ''
+    if not data['OPEN_SWITCH_PIN'] == '':
+        answer = input('Update the open pin value? (Y/N)')
+        if answer.upper() == 'Y':
+            data['OPEN_SWITCH_PIN'] = ''
+    if not data['CLOSED_SWITCH_PIN'] == '':
+        answer = input('Update the close pin value? (Y/N)')
+        if answer.upper() == 'Y':
+            data['CLOSED_SWITCH_PIN'] = ''
+
+    print('Using GPIO.BOARD layout');
+
+    while not data['RELAY_PIN'] == '':
+        try:
+            data['RELAY_PIN'] = int(
+                        input('Which pin was connected to the relay? '))
+        except ValueError:
+            print('Value must be an int.')
+    while not data['OPEN_SWITCH_PIN'] == '':
+        try:
+            data['OPEN_SWITCH_PIN'] = int(
+                        input('Which pin was connected to the OPEN switch? '))
+        except ValueError:
+            print('Value must be an int.')
+    while not data['CLOSED_SWITCH_PIN'] == '':
+        try:
+            data['CLOSED_SWITCH_PIN'] = int(
+                        input('Which pin was connected to the CLOSE switch? '))
+        except ValueError:
+            print('Value must be an int.')
+
+    # Only update API key if it does not already exist
+    if data['ACCESS_TOKEN'] == '':
+        print('Generating API key...')
+        for i in range(25):
+           apikey = apikey + ALPHANUM[randint(0,len(ALPHANUM)-1)]
+        data['ACCESS_TOKEN'] = apikey
+
     data['RELAY_PIN'] = relay_pin
     data['OPEN_SWITCH_PIN'] = open_pin
     data['CLOSED_SWITCH_PIN'] = close_pin
